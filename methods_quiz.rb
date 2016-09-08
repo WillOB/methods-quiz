e145566541043135f255ac479f8f230daed741d1
William Patrick O Brien <obrienwp@dcsdk12.org>
2016-09-08 13:55:43 -0600

    implement has_teen?

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -1,6 +1,9 @@
-	
+
 # TODO - write has_teen?
 
+def has_teen?(a, b, c)
+	(a - 13 <= 6 && a - 13 >= 0) || (b - 13 <= 6 && b - 13 >= 0) || (c - 13 <= 6 && c - 13 >= 0)
+end
 # TODO - write not_string
 
 # TODO - write icy_hot?
